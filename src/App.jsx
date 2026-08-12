e3d1c055ce3e42b6d8c2133fb092d28ae4d98ce3
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2026-08-12 17:28:50 +0000

    added text adding effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function Header(){
 
-
-  return(
+return(
     <>
       <header className='hd'>
         <p>About me</p>
@@ -15,11 +15,43 @@ function Header(){
   )
 }
 
+function Contents(){
+
+  let [txt,setTxt]=useState("_");
+  const wlcmTxt="Hi I'm Shweta"
+  
+  useEffect(()=>{
+
+    const timer = setInterval(()=>{
+      setTxt((prev)=>{
+        const currentTxt=prev.endsWith("_")?prev.slice(0,-1):prev;
+
+        if(currentTxt.length<wlcmTxt.length){
+          const nxt = wlcmTxt[currentTxt.length]
+          return currentTxt+nxt+"_";
+        }
+
+        clearInterval(timer);
+        return currentTxt;
+      })
+    },200)
+
+    return ()=>clearInterval(timer);
+  },[])
+
+  return(
+    <div className="contents">
+        <h1>{txt}</h1>
+      </div>
+  )
+}
+
 function App() {
 
   return (
     <>
       <Header></Header>
+      <Contents></Contents>
     </>
   )
 }
