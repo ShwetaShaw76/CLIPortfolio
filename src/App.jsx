592468af8a155a6d42682879d7dd90c7a91e3eae
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2026-08-12 16:49:20 +0000

    made the header and gave glowing effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
 import './App.css'
 
+function Header(){
+
+
+  return(
+    <>
+      <header className='hd'>
+        <p>About me</p>
+        <p>Projects</p>
+        <p>CLI Interface</p>
+      </header>
+    </>
+  )
+}
+
 function App() {
 
   return (
     <>
-    
+      <Header></Header>
     </>
   )
 }
