caf8329c45ab8d1923937a114d0c655bc5af0490
Shweta Shaw <189984154+ShwetaShaw76@users.noreply.github.com>
2026-08-13 19:22:46 +0000

    added project links,images and their github links

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,32 @@
 import './Project.css'
+import { useEffect, useState } from 'react'
+
+const ProjectLinks= [
+    "https://cleanit-crew.vercel.app/",
+    "https://khushi123.itch.io/corruption-lab-web",
+    "https://gaming-ball.vercel.app/",
+    "https://mystical-forest.vercel.app/",
+    "https://rhino-ride.vercel.app/",
+    "https://turquoise-os.vercel.app/"
+]
+
+const githubLinks= [
+    "https://github.com/ShwetaShaw76/CleanitCrew",
+    "https://github.com/ShwetaShaw76/CorruptionLab",
+    "https://github.com/ShwetaShaw76/GamingBall",
+    "https://github.com/ShwetaShaw76/mystical-forest",
+    "https://github.com/ShwetaShaw76/RhinoRide",
+    "https://github.com/ShwetaShaw76/TurquoiseOs"
+]
+
+const ProjectNames = [
+    "Cleanit Crew",
+    "Corruption Lab",
+    "Gaming Ball",
+    "Mystical Forest",
+    "Rhino Ride",
+    "Turquoise OS"
+]
 
 function Projects(){
     return(
@@ -6,8 +34,7 @@ function Projects(){
             <div className="prjct">
             <h1 className="hd2">My Projects</h1>
             <div className="cards">
-                <div className="card" id='c1'></div>
-                <div className="card" id='c2'></div>
+                
             </div>
             </div>
         </>
